ccf91d4aae75297c173daa68a81c795315af7917
AEMacBeath <alicemacbeath94@gmail.com>
2022-03-15 20:47:08 +0000

    Add loops to create the game board as per the difficulty selected

--- run.py
+++ run.py
@@ -2,10 +2,20 @@
 print('WELCOME TO BATTLESHIPS')
 
 # Player enter name - required
-username = input('please enter a username: ')
-print('Hello ' + username)
+"""username = input('please enter a username: ')
+print('Hello ' + username)"""
 
 # Board setup
+difficulty = input('Select a difficulty: ')
+
+if difficulty == 'h':
+    size = 10
+else:
+    size = 5
+
+for i in range(size):
+    print(" . " * size)
+
 # Game play
 # Score calculator
 # Winner / loser
